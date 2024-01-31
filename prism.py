d792bfd0dd1d415497b6d4f252750cc1a4e65c16
daGoodenough <dgoodenough@proton.me>
2024-01-30 18:01:00 -0800

    revert shebang

--- prism.py
+++ prism.py
@@ -1,4 +1,4 @@
-#!/nix/store/asiphbpiy2gmidfm3xbwcikayhs66289-python3-3.11.7/bin/python3
+#!/usr/bin/env python3
 from pyln.client import Plugin, RpcError
 from lib import Prism, Member, PrismBinding
 
