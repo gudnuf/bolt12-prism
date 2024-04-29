b5efb7d9f3d7eb3e449e4aa5de38872bb5dd6045
Derek Smith <derek@farscapian.com>
2024-04-29 15:42:26 -0500

    Move tests cases into tests folder.

--- tests/notes.py
+++ tests/notes.py
@@ -1,49 +1,31 @@
-import os
-from pyln.testing.fixtures import *  # noqa: F401,F403
-from pyln.client import Millisatoshi
 
-
-# plugin_path = os.path.join(os.path.dirname(__file__), "bolt12_prism.py")
-# plugin_opt = {'plugin': plugin_path}
-
-
-
-# this alone works
-def test_my_feature(node_factory):
-    # Start two lightning nodes
-    l1, l2 = node_factory.get_node(), node_factory.get_node()
-
-    # Example test to check if nodes are connected
-    l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
-
-    # Check the list of peers to confirm connection
-    assert len(l1.rpc.listpeers()['peers']) == 1
-    assert l1.rpc.listpeers()['peers'][0]['id'] == l2.info['id']
-
+    #time.sleep(500)
+    #except Exception as e:
+    #    return e
 
 
 # def test_bolt12_prism_dynamic_start(node_factory):
 #     # assert 0 == 1
-#     # l1 = node_factory.get_node(opts=plugin_opt)
-#     l1 = node_factory.get_node()
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.stop()
+#     # l2 = node_factory.get_node(opts=plugin_opt)
+#     l2 = node_factory.get_node()
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.stop()
 
 # def test_prism_list(node_factory):
-#     l1 = node_factory.get_node()
+#     l2 = node_factory.get_node()
 
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin bolt12_prism.py initialized.*")
-#     l1.rpc.plugin_stop(plugin_path)
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin bolt12_prism.py initialized.*")
-#     l1.stop()
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin bolt12_prism.py initialized.*")
+#     l2.rpc.plugin_stop(plugin_path)
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin bolt12_prism.py initialized.*")
+#     l2.stop()
 #     # # Then statically
-#     # l1.daemon.opts["plugin"] = plugin_path
-#     # l1.start()
+#     # l2.daemon.opts["plugin"] = plugin_path
+#     # l2.start()
 #     # # Start at 0 and 're-await' the two inits above. Otherwise this is flaky.
-#     # l1.daemon.logsearch_start = 0
-#     # l1.daemon.wait_for_logs(["Plugin rebalance initialized.*",
+#     # l2.daemon.logsearch_start = 0
+#     # l2.daemon.wait_for_logs(["Plugin rebalance initialized.*",
 #     #                          "Plugin rebalance initialized.*",
 #     #                          "Plugin rebalance initialized.*"])
 
@@ -62,28 +44,28 @@ def test_my_feature(node_factory):
 # def test_my_plugin(node_factory):
 #     # Create a node with your plugin enabled
 #     opts = {'plugin': '/path/to/your/plugin.py'}
-#     l1 = node_factory.get_node(options=opts)
+#     l2 = node_factory.get_node(options=opts)
     
 #     # Start writing tests, e.g., checking if the plugin loaded correctly
-#     assert 'myplugin' in l1.rpc.help()  # Replace 'myplugin' with your plugin's command
+#     assert 'myplugin' in l2.rpc.help()  # Replace 'myplugin' with your plugin's command
 
 
 
 # def test_rebalance_starts(node_factory):
-#     l1 = node_factory.get_node()
+#     l2 = node_factory.get_node()
 #     # Test dynamically
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     l1.rpc.plugin_stop(plugin_path)
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     l1.stop()
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     l2.rpc.plugin_stop(plugin_path)
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     l2.stop()
 #     # Then statically
-#     l1.daemon.opts["plugin"] = plugin_path
-#     l1.start()
+#     l2.daemon.opts["plugin"] = plugin_path
+#     l2.start()
 #     # Start at 0 and 're-await' the two inits above. Otherwise this is flaky.
-#     l1.daemon.logsearch_start = 0
-#     l1.daemon.wait_for_logs(["Plugin rebalance initialized.*",
+#     l2.daemon.logsearch_start = 0
+#     l2.daemon.wait_for_logs(["Plugin rebalance initialized.*",
 #                              "Plugin rebalance initialized.*",
 #                              "Plugin rebalance initialized.*"])
 
@@ -103,38 +85,38 @@ def test_my_feature(node_factory):
 
 
 # def test_rebalance_starts(node_factory):
-#     l1 = node_factory.get_node()
+#     l2 = node_factory.get_node()
 #     # Test dynamically
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     l1.rpc.plugin_stop(plugin_path)
-#     l1.rpc.plugin_start(plugin_path)
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     l1.stop()
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     l2.rpc.plugin_stop(plugin_path)
+#     l2.rpc.plugin_start(plugin_path)
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     l2.stop()
 #     # Then statically
-#     l1.daemon.opts["plugin"] = plugin_path
-#     l1.start()
+#     l2.daemon.opts["plugin"] = plugin_path
+#     l2.start()
 #     # Start at 0 and 're-await' the two inits above. Otherwise this is flaky.
-#     l1.daemon.logsearch_start = 0
-#     l1.daemon.wait_for_logs(["Plugin rebalance initialized.*",
+#     l2.daemon.logsearch_start = 0
+#     l2.daemon.wait_for_logs(["Plugin rebalance initialized.*",
 #                              "Plugin rebalance initialized.*",
 #                              "Plugin rebalance initialized.*"])
 
 
 # def test_rebalance_manual(node_factory, bitcoind):
-#     l1, l2, l3 = node_factory.line_graph(3, opts=plugin_opt)
-#     l1.daemon.logsearch_start = 0
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     nodes = [l1, l2, l3]
+#     l2, l3, l4 = node_factory.line_graph(3, opts=plugin_opt)
+#     l2.daemon.logsearch_start = 0
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     nodes = [l2, l3, l4]
 
 #     # form a circle so we can do rebalancing
-#     l3.connect(l1)
-#     l3.fundchannel(l1)
+#     l4.connect(l2)
+#     l4.fundchannel(l2)
 
 #     # get scids
-#     scid12 = l1.get_channel_scid(l2)
-#     scid23 = l2.get_channel_scid(l3)
-#     scid31 = l3.get_channel_scid(l1)
+#     scid12 = l2.get_channel_scid(l3)
+#     scid23 = l3.get_channel_scid(l4)
+#     scid31 = l4.get_channel_scid(l2)
 #     scids = [scid12, scid23, scid31]
 
 #     # wait for each others gossip
@@ -144,7 +126,7 @@ def test_my_feature(node_factory):
 #             n.wait_channel_active(scid)
 
 #     # check we can do an auto amount rebalance
-#     result = l1.rpc.rebalance(scid12, scid31)
+#     result = l2.rpc.rebalance(scid12, scid31)
 #     print(result)
 #     assert result['status'] == 'complete'
 #     assert result['outgoing_scid'] == scid12
@@ -156,13 +138,13 @@ def test_my_feature(node_factory):
 #     wait_for_all_htlcs(nodes)
 
 #     # check that channels are now balanced
-#     c12 = l1.rpc.listpeerchannels(l2.info['id'])['channels'][0]
-#     c13 = l1.rpc.listpeerchannels(l3.info['id'])['channels'][0]
+#     c12 = l2.rpc.listpeerchannels(l3.info['id'])['channels'][0]
+#     c13 = l2.rpc.listpeerchannels(l4.info['id'])['channels'][0]
 #     assert abs(0.5 - (Millisatoshi(c12['to_us_msat']) / Millisatoshi(c12['total_msat']))) < 0.01
 #     assert abs(0.5 - (Millisatoshi(c13['to_us_msat']) / Millisatoshi(c13['total_msat']))) < 0.01
 
 #     # check we can do a manual amount rebalance in the other direction
-#     result = l1.rpc.rebalance(scid31, scid12, '250000000msat')
+#     result = l2.rpc.rebalance(scid31, scid12, '250000000msat')
 #     assert result['status'] == 'complete'
 #     assert result['outgoing_scid'] == scid31
 #     assert result['incoming_scid'] == scid12
@@ -170,41 +152,41 @@ def test_my_feature(node_factory):
 #     assert result['received'] == '250000000msat'
 
 #     # briefly check rebalancereport works
-#     report = l1.rpc.rebalancereport()
+#     report = l2.rpc.rebalancereport()
 #     assert report.get('rebalanceall_is_running') is False
 #     assert report.get('total_successful_rebalances') == 2
 
 
 # def test_rebalance_all(node_factory, bitcoind):
-#     l1, l2, l3 = node_factory.line_graph(3, opts=plugin_opt)
-#     l1.daemon.logsearch_start = 0
-#     l1.daemon.wait_for_log("Plugin rebalance initialized.*")
-#     nodes = [l1, l2, l3]
+#     l2, l3, l4 = node_factory.line_graph(3, opts=plugin_opt)
+#     l2.daemon.logsearch_start = 0
+#     l2.daemon.wait_for_log("Plugin rebalance initialized.*")
+#     nodes = [l2, l3, l4]
 
 #     # check we get an error if theres just one channel
-#     result = l1.rpc.rebalanceall()
+#     result = l2.rpc.rebalanceall()
 #     assert result['message'] == 'Error: Not enough open channels to rebalance anything'
 
-#     # now we add another 100% outgoing liquidity to l1 which does not help
-#     l4 = node_factory.get_node()
-#     l1.connect(l4)
-#     l1.fundchannel(l4)
+#     # now we add another 100% outgoing liquidity to l2 which does not help
+#     l5 = node_factory.get_node()
+#     l2.connect(l5)
+#     l2.fundchannel(l5)
 
 #     # test this is still not possible
-#     result = l1.rpc.rebalanceall()
+#     result = l2.rpc.rebalanceall()
 #     assert result['message'] == 'Error: Not enough liquidity to rebalance anything'
 
-#     # remove l4 it does not distort further testing
-#     l1.rpc.close(l1.get_channel_scid(l4))
+#     # remove l5 it does not distort further testing
+#     l2.rpc.close(l2.get_channel_scid(l5))
 
 #     # now we form a circle so we can do actually rebalanceall
-#     l3.connect(l1)
-#     l3.fundchannel(l1)
+#     l4.connect(l2)
+#     l4.fundchannel(l2)
 
 #     # get scids
-#     scid12 = l1.get_channel_scid(l2)
-#     scid23 = l2.get_channel_scid(l3)
-#     scid31 = l3.get_channel_scid(l1)
+#     scid12 = l2.get_channel_scid(l3)
+#     scid23 = l3.get_channel_scid(l4)
+#     scid31 = l4.get_channel_scid(l2)
 #     scids = [scid12, scid23, scid31]
 
 #     # wait for each others gossip
@@ -212,17 +194,17 @@ def test_my_feature(node_factory):
 #     wait_for_all_active(nodes, scids)
 
 #     # check that theres nothing to stop when theres nothing to stop
-#     result = l1.rpc.rebalancestop()
+#     result = l2.rpc.rebalancestop()
 #     assert result['message'] == "No rebalance is running, nothing to stop."
 
 #     # check the rebalanceall starts
-#     result = l1.rpc.rebalanceall(feeratio=5.0)  # we need high fees to work
+#     result = l2.rpc.rebalanceall(feeratio=5.0)  # we need high fees to work
 #     assert result['message'].startswith('Rebalance started')
-#     l1.daemon.wait_for_logs([f"tries to rebalance: {scid12} -> {scid31}",
+#     l2.daemon.wait_for_logs([f"tries to rebalance: {scid12} -> {scid31}",
 #                              f"Automatic rebalance finished"])
 
 #     # check additional calls to stop return 'nothing to stop' + last message
-#     result = l1.rpc.rebalancestop()['message']
+#     result = l2.rpc.rebalancestop()['message']
 #     assert result.startswith("No rebalance is running, nothing to stop. "
 #                              "Last 'rebalanceall' gave: Automatic rebalance finished")
 
@@ -230,12 +212,12 @@ def test_my_feature(node_factory):
 #     wait_for_all_htlcs(nodes)
 
 #     # check that channels are now balanced
-#     c12 = l1.rpc.listpeerchannels(l2.info['id'])['channels'][0]
-#     c13 = l1.rpc.listpeerchannels(l3.info['id'])['channels'][0]
+#     c12 = l2.rpc.listpeerchannels(l3.info['id'])['channels'][0]
+#     c13 = l2.rpc.listpeerchannels(l4.info['id'])['channels'][0]
 #     assert abs(0.5 - (Millisatoshi(c12['to_us_msat']) / Millisatoshi(c12['total_msat']))) < 0.01
 #     assert abs(0.5 - (Millisatoshi(c13['to_us_msat']) / Millisatoshi(c13['total_msat']))) < 0.01
 
 #     # briefly check rebalancereport works
-#     report = l1.rpc.rebalancereport()
+#     report = l2.rpc.rebalancereport()
 #     assert report.get('rebalanceall_is_running') is False
 #     assert report.get('total_successful_rebalances') == 2
---
+++ tests/test_bolt12_prism.py
@@ -0,0 +1,31 @@
+import os
+from pyln.testing.fixtures import *  # noqa: F401,F403
+from pyln.client import Millisatoshi
+
+plugin_path = os.path.join(os.path.dirname(__file__), '../bolt12-prism.py')
+plugin_opt = {'plugin': plugin_path}
+
+# spin up a network
+def test_basic_test(node_factory):
+    # Start two lightning nodes
+    l1, l2, l3, l4, l5 = node_factory.get_node(), node_factory.get_node(), node_factory.get_node(), node_factory.get_node(), node_factory.get_node()
+
+    # Connect the nodes in a prism layout.
+    l1.rpc.connect(l2.info['id'], 'localhost', l2.port) # Alice -> Bob
+    l2.rpc.connect(l3.info['id'], 'localhost', l3.port) # Bob -> Carol
+    l2.rpc.connect(l4.info['id'], 'localhost', l4.port) # Bob -> Dave
+    l2.rpc.connect(l5.info['id'], 'localhost', l5.port) # Bob -> Erin
+
+    # Check the list of peers to confirm connection
+    # Alice -> Bob
+    assert len(l1.rpc.listpeers()['peers']) == 1
+    assert l1.rpc.listpeers()['peers'][0]['id'] == l2.info['id']
+
+    # Bob -> all others
+    assert len(l2.rpc.listpeers()['peers']) == 4
+
+    #try:
+    assert l2.rpc.plugin_start(plugin_path)
+    assert l2.rpc.plugin_list()
+    #assert l2.rpc.prism_list()
+    assert l2.rpc.plugin_stop(plugin_path)
