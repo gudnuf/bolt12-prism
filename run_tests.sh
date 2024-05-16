2a05b58e73d0f94cf63e9cf4ad210d4a02080b4c
Derek Smith <derek@farscapian.com>
2024-05-16 12:18:20 -0400

    Improve run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,8 @@ set -eu
 
 # first build the image if it doesn't exit.
 BOLT12_PRISM_IMAGE_NAME="bolt12-prism-pyln-testing:v24.02.2"
-if ! docker image inspect "$BOLT12_PRISM_IMAGE_NAME" &>/dev/null; then
-    docker buildx build . -t "$BOLT12_PRISM_IMAGE_NAME" --load
-fi
+docker buildx build . -t "$BOLT12_PRISM_IMAGE_NAME" --load
 
 if ! docker ps -a | grep bolt12-tester >> /dev/null; then
-    docker run --rm --name bolt12-tester -it bolt12-prism-pyln-testing:v24.02.2
+    docker run --rm --name bolt12-tester -it "$BOLT12_PRISM_IMAGE_NAME"
 fi
